5425c49f82b26a5aaab1515832ef497039cfa503
Alexander <a-l-e-k-s@yandex.ru>
2021-04-12 21:11:52 +0300

    Initial Commit

---
+++ HW3.6Shapes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3.6Shapes
+//
+//  Created by Alexander Konovalov on 12.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW3.6Shapes/HW3_6ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_6ShapesApp.swift
+//  HW3.6Shapes
+//
+//  Created by Alexander Konovalov on 12.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW3_6ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
