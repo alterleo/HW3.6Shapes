97308b2cd4b8d47428773cf3b924c01f07805af4
Alexander <a-l-e-k-s@yandex.ru>
2021-04-13 18:43:02 +0300

    some refactoring

--- HW3.6Shapes/RainbowView.swift
+++ HW3.6Shapes/RainbowView.swift
@@ -20,15 +20,30 @@ struct RainbowView: View {
                 ForEach((0..<5)) { indx in
                     Circle()
                         .trim(from: 0.5, to: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
-                        .stroke(Color.gray, style: StrokeStyle(lineWidth: 20, lineCap: .round, lineJoin: .round))
-                        .frame(width: CGFloat(rainbSize-indx*41), height: CGFloat(rainbSize-indx*41))
+                        .stroke(Color.gray,
+                                style: StrokeStyle(
+                                    lineWidth: 20,
+                                    lineCap: .round,
+                                    lineJoin: .round)
+                        )
+                        .frame(width: CGFloat(rainbSize-indx*41),
+                               height: CGFloat(rainbSize-indx*41)
+                        )
                 }
                 ForEach((0..<5)) { indx in
                     Circle()
                         .trim(from: CGFloat(toPoint), to: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
-                        .stroke(rainbColors[indx], style: StrokeStyle(lineWidth: 20, lineCap: .round, lineJoin: .round))
-                        .frame(width: CGFloat(rainbSize-indx*41), height: CGFloat(rainbSize-indx*41))
-                        .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true))
+                        .stroke(rainbColors[indx],
+                                style: StrokeStyle(
+                                    lineWidth: 20,
+                                    lineCap: .round,
+                                    lineJoin: .round)
+                        )
+                        .frame(width: CGFloat(rainbSize-indx*41),
+                               height: CGFloat(rainbSize-indx*41)
+                        )
+                        .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true)
+                        )
                 }
             }
         }
